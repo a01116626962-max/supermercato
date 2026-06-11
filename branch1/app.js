a2be6952e7d39d5703a543a0f34652cb19d5a448
a01116626962-max <a01116626962@gmail.com>
2026-06-12 02:07:17 +0300

    Update app.js

--- branch1/app.js
+++ branch1/app.js
@@ -6,15 +6,6 @@ import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, query, where
 
 // ⚠️ ضع كود الـ Config الخاص بك هنا ⚠️
 const firebaseConfig = {
-    <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyB20o1dChYXfgBmw0cro4qkdJK4zdcnhBs",
     authDomain: "negozio-43e16.firebaseapp.com",
